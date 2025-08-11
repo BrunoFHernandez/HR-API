0dbcab9e064257c05ede7864c2d66caf1261c84a
BrunoFHernandez <brunofelipehernandez@gmail.com>
2025-08-10 23:10:29 -0400

    feat: configura conexao com o DB

---
+++ src/data-source.ts
@@ -0,0 +1,15 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { User } from './entity/user';
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT || 3306),
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'hr_api',
+  synchronize: true, 
+  logging: false,
+  entities: [User],
+});
